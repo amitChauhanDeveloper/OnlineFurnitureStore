d003785aea2c066f2569c1381e6e9ad47008fdb4
amit chauhan <amitharidwarchauhan@gmail.com>
2022-11-24 23:53:02 +0530

    first commit

---
+++ backend/data.js
@@ -0,0 +1,76 @@
+import bcrypt from 'bcryptjs';
+
+const data = {
+  users: [
+    {
+      name: 'IKEA',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: true,
+      isSeller: true,
+      seller: {
+        name: 'IKEA',
+        logo: '/images/logo1.png',
+        description: 'best seller',
+        rating: 4.5,
+        numReviews: 120,
+      },
+    },
+    {
+      name: 'Amit',
+      email: 'amit@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: false,
+    },
+  ],
+  products: [
+    {
+      _id: '1',
+      name: 'Wood King Bed',
+      category: 'Beds',
+      image: '/images/f1.jpg',
+      price: 12816,
+      brand: ' IKEA',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 6,
+      description: 'high quality product',
+    },
+    {
+      _id: '2',
+      name: 'Metal Double Bed',
+      category: 'Beds',
+      image: '/images/f2.jpg',
+      price: 6290,
+      brand: ' IKEA',
+      rating: 4.2,
+      numReviews: 5,
+      countInStock: 10,
+      description: 'high quality product',
+    },
+    {
+      _id: '3',
+      name: 'King Box Bed',
+      category: 'Beds',
+      image: '/images/f3.jpg',
+      price: 28380,
+      brand: ' IKEA',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 6,
+      description: 'high quality product',
+    }, {
+      _id: '4',
+      name: 'Drawer Storage For Bedroom',
+      category: 'Beds',
+      image: '/images/f4.jpg',
+      price: 20249,
+      brand: ' IKEA',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 6,
+      description: 'high quality product',
+    },
+  ],
+};
+export default data;
---
+++ frontend/src/utils.js
@@ -0,0 +1,43 @@
+export const prices = [
+  {
+    name: 'Any',
+    min: 1000,
+    max: 100000,
+  },
+  {
+    name: `₹ 1000 to ₹ 5000`,
+    min: 1000,
+    max: 5000,
+  },
+  {
+    name: `₹ 5001 to ₹ 10000`,
+    min: 5001,
+    max: 10000,
+  },
+  {
+    name: `₹ 10001 to ₹ 100000`,
+    min: 10001,
+    max: 100000,
+  },
+];
+export const ratings = [
+  {
+    name: '4stars & up',
+    rating: 4,
+  },
+
+  {
+    name: '3stars & up',
+    rating: 3,
+  },
+
+  {
+    name: '2stars & up',
+    rating: 2,
+  },
+
+  {
+    name: '1stars & up',
+    rating: 1,
+  },
+];
